dab4108c3f7b37951bbec5f4dd97d2f20b9ac3bf
Prem <premsoni0469@gmail.com>
2024-07-29 11:31:15 +0530

    Program for Addition

---
+++ Addition.java
@@ -0,0 +1,15 @@
+class Operation{
+int a,b,ans;
+void add(){
+ans=a+b;
+System.out.println(ans);
+}
+}
+ class Addition{
+public static void main(String[] args){
+Operation obj1=new Operation();
+obj1.a=10;
+obj.2=20;
+0bj1.add();
+}
+}
